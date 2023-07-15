ab37a1fea71ddb46ae227e79a1cb22e99d7c0e14
Tamzin <transgenderdotorg@gmail.com>
2023-07-14 23:11:03 -0700

    Fix search

--- src/app.tsx
+++ src/app.tsx
@@ -4,16 +4,14 @@ import "./app.css";
 import logoUrl from "./assets/logo.svg";
 import appData from "./app-data.json";
 import { LinkList } from "./link-list";
-
-const PostList = (posts: any) => {
-  const { results, searchValue, setSearchValue } = useSearch<Post>({
-    dataSet: appData.links,
-    keys: ['title', 'description', 'url'],
-  })
-}
-export default PostList
+import useSearch from "./useSearch";
 
 const App = () => {
+  const { results, searchValue, setSearchValue } = useSearch<Post>({
+    dataSet: appData.links,
+    keys: ["title", "description", "url"],
+  });
+
   return (
     <div className="page one-column">
       <div className="column">
@@ -27,30 +25,30 @@ const App = () => {
         <LinkList links={appData.links} />
         <form>
           <label>Search:</label>
-          <input type="searchValue" id="searchValue" name="searchValue"></input>
+          <input
+            type="searchValue"
+            id="searchValue"
+            name="searchValue"
+            onChange={(e) => setSearchValue(e.currentTarget.value)}
+          ></input>
         </form>
-        {PostList}
+        {JSON.stringify(results)}
       </div>
     </div>
   );
 };
 
-
-
 ReactDOM.render(<App />, document.getElementById("app"));
 
-import useSearch from './useSearch'
 // ...
 
 interface Post {
-	title: any
-	description: any
-	url: any
-	// ... other fields
+  title: any;
+  description: any;
+  url: any;
+  // ... other fields
 }
 
+// Search bar here
 
-	// Search bar here
-
-	// Render the results accordingly
-
+// Render the results accordingly
--- src/useSearch.tsx
+++ src/useSearch.tsx
@@ -1,38 +1,38 @@
-import { useState, useMemo } from 'react'
-import Fuse from 'fuse.js'
+import { useState, useMemo } from "react";
+import Fuse from "fuse.js";
 
 interface IUseSearchProps<T> {
-  dataSet: T[]
-  keys: string[]
+  dataSet: T[];
+  keys: string[];
 }
 
-const SCORE_THRESHOLD = 0.4
+const SCORE_THRESHOLD = 0.4;
 
 export default function useSearch<T>({ dataSet, keys }: IUseSearchProps<T>) {
-  const [searchValue, setSearchValue] = useState('')
+  const [searchValue, setSearchValue] = useState("");
 
   const fuse = useMemo(() => {
     const options = {
       includeScore: true,
       keys,
-    }
+    };
 
-    return new Fuse(dataSet, options)
-  }, [dataSet, keys])
+    return new Fuse(dataSet, options);
+  }, [dataSet, keys]);
 
   const results = useMemo(() => {
-    if (!searchValue) return dataSet
+    if (!searchValue) return dataSet;
 
-    const searchResults = fuse.search("trans")
+    const searchResults = fuse.search(searchValue);
 
     return searchResults
       .filter((fuseResult: any) => fuseResult.score < SCORE_THRESHOLD)
-      .map((fuseResult) => fuseResult.item)
-  }, [fuse, searchValue, dataSet])
+      .map((fuseResult) => fuseResult.item);
+  }, [fuse, searchValue, dataSet]);
 
   return {
     searchValue,
     setSearchValue,
     results,
-  }
+  };
 }
